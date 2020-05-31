20b13fc3142e79681fcb9edce2926971c7377dd3
AntonSemenov87 <Anthony04061987@gmail.com>
2020-05-31 10:45:42 -0500

    first push

---
+++ src/Hello.java
@@ -0,0 +1,23 @@
+public class Hello {
+
+    public static void main(String [] args) {
+
+        int myFirstNumber = (5 + 10) + (2 * 10);
+        System.out.println("myFirstNumber = " + myFirstNumber);
+
+        int mySecondNumber = 12;
+
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
+
+
+    }
+
+}
---
+++ src/PrimitiveTypes.java
@@ -0,0 +1,16 @@
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+
+        int myValue = 1000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer min value is: " + myMinIntValue);
+        System.out.println("Integer max value is: " + myMaxIntValue);
+
+        int myMaxIntTest = 2147483648;
+
+
+
+    }
+}
