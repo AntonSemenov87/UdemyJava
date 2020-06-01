bfa26003a8b6e784236aca03493502057b38e622
AntonSemenov87 <Anthony04061987@gmail.com>
2020-06-01 12:06:16 -0500

    updates

---
+++ src/FirstSteps/KeywordsExpressions.java
@@ -0,0 +1,22 @@
+package FirstSteps;
+
+public class KeywordsExpressions {
+    public static void main(String[] args) {
+        int int2 = 5;
+        // false, true, null
+
+        // a mile is equal to 1.6409344 km
+        double kilometers = (100 * 1.609344);
+        // expression components: variable, values and operators (kilometers = (100 * 1.609344))
+        // statement:  double kilometers = (100 * 1.609344);
+
+        int highScore = 50; // statement
+
+        if (highScore == 50) { // expression
+            System.out.println("This is an expression"); // expression
+            highScore = 0; // expression
+        }
+
+
+    }
+}
