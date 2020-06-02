0adf0ff6bcc26b91204d69a26124306464578e34
AntonSemenov87 <Anthony04061987@gmail.com>
2020-06-01 23:08:01 -0500

    updates

--- src/Section4/Methods.java
+++ src/Section4/Methods.java
@@ -3,15 +3,23 @@ package Section4;
 public class Methods {
     public static void main(String[] args) {
 
+        System.out.println("Running first example:");
 //        boolean gameOver = true;
 //        int score = 800;
 //        int levelCompleted = 5;
 //        int bonus = 100;
-
-        System.out.println("Running first example:");
         calculateScore(true, 800, 5, 100);
+        // or: calculateScore(gameOver, score, levelCompleted, bonus);
+
+
+
         System.out.println("Running second example:");
+//        gameOver = true;
+//        score = 800;
+//        levelCompleted = 5;
+//        bonus = 100;
         calculateScore(true, 10_000, 8, 200);
+        // or: calculateScore(gameOver, score, levelCompleted, bonus);
 
 
         System.out.println("Now this is last thing to be executed");
