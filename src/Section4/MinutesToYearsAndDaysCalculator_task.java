20a9f58daf5cd2c002f931f0c036a80b05d3cb18
AntonSemenov87 <Anthony04061987@gmail.com>
2020-06-11 20:55:37 -0500

    updates

--- src/Section4/MinutesToYearsAndDaysCalculator_task.java
+++ src/Section4/MinutesToYearsAndDaysCalculator_task.java
@@ -1,4 +1,16 @@
 package Section4;
 
 public class MinutesToYearsAndDaysCalculator_task {
+    private static final String INVALID_MINUTES_VALUE = "Invalid Value";
+
+    public static void printYearsAndDays (long minutes) {
+            if (minutes >= 0) {
+                int year = (int) minutes / 525600;
+                int remainingMinutes = (int) minutes % 525600;
+                int days = remainingMinutes / 1440;
+                System.out.println(minutes + " min = " + year + " y and " + days + " d");
+            } else {
+                System.out.println(INVALID_MINUTES_VALUE);
+            }
+    }
 }
