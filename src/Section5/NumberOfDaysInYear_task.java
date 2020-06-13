18cb8edbc7d376fb08db42ac312aa84782e9d1c4
AntonSemenov <Anthony04061987@gmail.com>
2020-06-12 20:33:56 -0500

    updates

--- src/Section5/NumberOfDaysInYear_task.java
+++ src/Section5/NumberOfDaysInYear_task.java
@@ -20,26 +20,26 @@ public class NumberOfDaysInYear_task {
     }
 
     public static int getDaysInMonth (int month, int year) {
-        int x = 31;
+        int days = 31;
         if ((month < 1 || month > 12) || (year < 1 || year > 9999)) {
             return -1;
         } else {
 
             switch (month) {
                 case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-                    x = 31;
+                    days = 31;
                     break;
                 case 4: case 6: case 9: case 11:
-                    x = 30;
+                    days = 30;
                     break;
                 case 2:
                     if (isLeapYear(year)) {
-                        x = 29;
+                        days = 29;
                     } else {
-                        x = 28;
+                        days = 28;
                     }
             }
         }
-        return x;
+        return days;
     }
 }
