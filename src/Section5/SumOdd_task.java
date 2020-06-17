ad79cb831a3042607a1210ebbb4e22d51faeb0bf
AntonSemenov87 <Anthony04061987@gmail.com>
2020-06-16 23:24:13 -0500

    updates

--- src/Section5/SumOdd_task.java
+++ src/Section5/SumOdd_task.java
@@ -3,27 +3,28 @@ package Section5;
 public class SumOdd_task {
     public static void main(String[] args) {
 
-        System.out.println(sumOdd(100, 100));
+        System.out.println(sumOdd(100, 1000));
 
     }
 
-    public static boolean isOdd (int number) {
-        if (number < 0 && number % 2 != 0) {
-            return false;
-        } else {
+    public static boolean isOdd(int number) {
+        if (number > 0 && number % 2 != 0) {
             return true;
         }
+        return false;
     }
-    public static int sumOdd (int start, int end) {
-        if (start <= 0 || end <= 0) {
+
+    public static int sumOdd(int start, int end) {
+        if (start >= 0 && end >= 0 && end >= start) {
+            int sum = 0;
+            for (int i = start; i <= end; i++) {
+                if (isOdd(i)) {
+                    sum += i;
+                }
+            }
+            return sum;
+        } else {
             return -1;
         }
-        int sum = 0;
-        for (int i = start; i <= end; i++) {
-            if (isOdd(i)) {
-                sum += i;
-            }
-        }
-        return sum;
     }
 }
