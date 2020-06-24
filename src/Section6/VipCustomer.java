3c93f87c1cb0d9b2a91f4ebcb80de5f3f5ea283f
AntonSemenov87 <Anthony04061987@gmail.com>
2020-06-23 23:53:34 -0500

    updates

--- src/Section6/VipCustomer.java
+++ src/Section6/VipCustomer.java
@@ -4,4 +4,14 @@ public class VipCustomer {
     private String name;
     private double creditLimit;
     private String emailAddress;
+
+    // 1st empty Constructor
+    public VipCustomer () {
+        this ("default name", 0.00, "default@address.com");
+    }
+
+    public VipCustomer (String name, double creditLimit) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+    }
 }
