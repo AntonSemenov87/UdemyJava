95ff515d3d8fdc0db8f41ae0cf136e6b51982f44
AntonSemenov <Anthony04061987@gmail.com>
2020-06-24 20:35:59 -0500

    updates

--- src/OCAstuff/Zoo.java
+++ src/OCAstuff/Zoo.java
@@ -6,5 +6,15 @@ public class Zoo {
     public static void main(String [] args) {
         Random r = new Random();
         System.out.println(r.nextInt(2));
+
+        StringBuffer sbuf = new StringBuffer();
+        System.out.println("Capacity of StringBuffer is = " + sbuf.capacity());
+        System.out.println("Length of StringBuffer is = " + sbuf.length());
+
+        StringBuffer sb = new StringBuffer("Jupiter is awesome!");
+        System.out.println(sbuf);
+        sb.append(" So is Saturn!");
+        System.out.println(sb);
+
     }
 }
