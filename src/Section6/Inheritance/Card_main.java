b5899489861e2685546b3aa9c5db16ae0d75dcf6
AntonSemenov87 <Anthony04061987@gmail.com>
2020-07-01 18:01:54 -0500

    updates

--- src/Section6/Inheritance/Card_main.java
+++ src/Section6/Inheritance/Card_main.java
@@ -2,10 +2,10 @@ package Section6.Inheritance;
 
 public class Card_main {
     public static void main(String[] args) {
-        CreditCard card1 = new CreditCard("Anton", "Semenov", "123456", 2021, 345, "Chicago",
+        CreditCard card1 = new CreditCard("Anton", "Sem", "123456", 2021, 345, "Chicago",
                 "credit", "Visa");
 
-        DebitCard card2 = new DebitCard("Vita Bezruchko","12123232", "13132454", 2023, 323,
+        DebitCard card2 = new DebitCard("Vita Bez","12123232", "13132454", 2023, 323,
                 "Chicago", "MC");
 
 
