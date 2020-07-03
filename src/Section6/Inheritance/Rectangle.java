b618f7002e232affd3d69a5e9c714808bdd7f347
AntonSemenov87 <Anthony04061987@gmail.com>
2020-07-02 23:50:35 -0500

    updates

---
+++ src/Section6/Inheritance/Rectangle.java
@@ -0,0 +1,20 @@
+package Section6.Inheritance;
+
+public class Rectangle {
+    private double width;
+    private double length;
+
+    public Rectangle (double width, double length) {
+        if (length < 0) {
+            this.length = 0;
+        } else {
+            this.length = length;
+        }
+
+        if (width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+    }
+}
