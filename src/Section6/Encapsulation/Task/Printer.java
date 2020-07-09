3dbe9872bb7430980d2467fb31f67b47ae6e2b96
AntonSemenov87 <Anthony04061987@gmail.com>
2020-07-08 23:51:04 -0500

    updates

--- src/Section6/Encapsulation/Task/Printer.java
+++ src/Section6/Encapsulation/Task/Printer.java
@@ -5,4 +5,23 @@ public class Printer {
     private int pagesPrinted;
     private boolean duplex;
 
+    public Printer(int tonerLevel, boolean duplex) {
+        if (tonerLevel >= 1 && tonerLevel <= 100) {
+            this.tonerLevel = tonerLevel;
+        } else {
+            this.tonerLevel = -1;
+        }
+        this.tonerLevel = tonerLevel;
+        this.duplex = duplex;
+        this.pagesPrinted = 0;
+    }
+
+    public int addToner (int tonerAmount) {
+        if (tonerLevel > 0 && tonerLevel <= 100) {
+            return this.tonerLevel += tonerAmount;
+        } else {
+            return -1;
+        }
+    }
+
 }
