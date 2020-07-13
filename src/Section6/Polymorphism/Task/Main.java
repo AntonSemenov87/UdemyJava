7de713a181a3674ef690b0eb634f13c6a943bb1e
AntonSemenov87 <Anthony04061987@gmail.com>
2020-07-12 23:52:52 -0500

    updates

--- src/Section6/Polymorphism/Task/Main.java
+++ src/Section6/Polymorphism/Task/Main.java
@@ -57,5 +57,74 @@ class Mitsubishi extends Car {
         return "Mitsubishi -> brake()";
     }
 }
+class Ford extends Car {
+
+    public Ford(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Ford -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Ford -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Ford -> brake()";
+    }
+}
+
+class Porsche extends Car {
+
+    public Porsche(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi -> brake()";
+    }
+}
+
+
 public class Main {
+    public static void main(String[] args) {
+        Car car = new Car(8, "Base car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRW 4WD");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+        Ford ford = new Ford(6, "Ford Falcon");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+
+        Porsche porsche = new Porsche(8, "Carrera 911");
+        System.out.println(porsche.startEngine());
+        System.out.println(porsche.accelerate());
+        System.out.println(porsche.brake());
+
+
+
+    }
 }
