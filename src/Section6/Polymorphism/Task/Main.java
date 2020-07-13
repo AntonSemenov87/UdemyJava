882f58c7a45aab3e6b39f6bdb4f894cfa403bbbd
AntonSemenov87 <Anthony04061987@gmail.com>
2020-07-12 23:28:58 -0500

    updates

--- src/Section6/Polymorphism/Task/Main.java
+++ src/Section6/Polymorphism/Task/Main.java
@@ -36,5 +36,26 @@ class Car {
 
 }
 
+class Mitsubishi extends Car {
+
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi -> brake()";
+    }
+}
 public class Main {
 }
