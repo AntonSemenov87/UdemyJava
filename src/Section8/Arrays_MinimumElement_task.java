74a770c84291c53202d75a8ab4e5004827c702e3
AntonSemenov87 <Anthony04061987@gmail.com>
2020-07-14 22:52:08 -0500

    updates

---
+++ src/Section8/Arrays_MinimumElement_task.java
@@ -0,0 +1,37 @@
+package Section8;
+
+import java.util.Scanner;
+
+public class Arrays_MinimumElement_task {
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Enter count:");
+        int count = scanner.nextInt();
+        scanner.nextLine();
+
+        int [] myArray = readIntegers(count);
+        System.out.println(findMin(myArray));
+
+    }
+    public static int [] readIntegers (int count) {
+        System.out.println("Enter " + count + " numbers.\r");
+        int [] array = new int[count];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    private static int findMin (int [] array) {
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+}
