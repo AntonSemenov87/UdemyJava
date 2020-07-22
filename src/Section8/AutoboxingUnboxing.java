2595f9fe73e45868ba2b6098b1f393f89f8a482a
AntonSemenov87 <Anthony04061987@gmail.com>
2020-07-21 22:45:52 -0500

    autoboxing and unboxing

--- src/Section8/AutoboxingUnboxing.java
+++ src/Section8/AutoboxingUnboxing.java
@@ -41,6 +41,22 @@ public class AutoboxingUnboxing {
             System.out.println(i + " --> " + intArrayList.get(i).intValue());
         }
 
+        Integer myIntValue = 56; // Integer.valueOf(56);
+        int myInt = myIntValue; // myInt.intValue()
+
+
+
+
+
+        ArrayList<Double> myDoubleValues = new ArrayList<Double>();
+        for (double dbl = 0.0; dbl < 10.0; dbl+=0.5) {
+            myDoubleValues.add(Double.valueOf(dbl));
+        }
+        for (int i = 0; i < myDoubleValues.size(); i++) {
+            double value = myDoubleValues.get(i).doubleValue();
+            System.out.println(i + " --> " + value);
+        }
+
 
     }
 }
