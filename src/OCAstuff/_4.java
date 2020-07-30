bf673f3ed840a0c1cbd208d1e346120e61fa2253
AntonSemenov <Anthony04061987@gmail.com>
2020-07-30 13:14:30 -0500

    some practice

---
+++ src/OCAstuff/_4.java
@@ -0,0 +1,14 @@
+package OCAstuff;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class _4 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2012, 1, 11);
+        Period period = Period.ofMonths(2);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yy");
+        System.out.println(formatter.format(date.minus(period)));
+    }
+}
---
+++ src/OCAstuff/_6.java
@@ -0,0 +1,14 @@
+package OCAstuff;
+
+public class _6 {
+    public static void main(String[] args) {
+        Boolean b1 = new Boolean("tRuE");
+        Boolean b2 = new Boolean("fAlSe");
+        Boolean b3 = new Boolean("abc");
+        Boolean b4 = null;
+        Boolean b5 = new Boolean("TRUE");
+        Boolean b6 = new Boolean("false1");
+        System.out.println(b1 + ":" + b2 + ":" + b3 + ":" + b4);
+        System.out.println(b5+":"+b6);
+    }
+}
