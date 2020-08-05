3d3c54c58d8fe6a63d2b16c28afe33c30dbb4775
AntonSemenov87 <Anthony04061987@gmail.com>
2020-08-04 21:25:35 -0500

    stuff

--- src/RandomStuff/Break_example.java
+++ src/RandomStuff/Break_example.java
@@ -1,4 +1,25 @@
 package RandomStuff;
 
 public class Break_example {
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.println("Hi");
+            break;
+        }
+
+        int i = 0;
+        while (i < 3) {
+            System.out.println("Hello");
+            i++;
+            break;
+        }
+
+        int [] numbers = {10, 20, 30, 40, 50};
+        for (int j = 0; j < numbers.length; j++) {
+            if (numbers[j] == 30) break;
+            System.out.println(numbers[j]);
+        }
+
+    }
 }
