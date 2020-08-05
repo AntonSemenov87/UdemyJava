fc816d7188128bbb00c2e11690fec35fd01cfd69
AntonSemenov87 <Anthony04061987@gmail.com>
2020-08-04 21:49:06 -0500

    string quick review

---
+++ src/RandomStuff/HashMap_practice/Break_nestedLoop.java
@@ -0,0 +1,12 @@
+package RandomStuff.HashMap_practice;
+
+public class Break_nestedLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println(i + ", " + j);
+                break;
+            }
+        }
+    }
+}
---
+++ src/RandomStuff/Strings_usage.java
@@ -0,0 +1,16 @@
+package RandomStuff;
+
+public class Strings_usage {
+    public static void main(String[] args) {
+        String name = "Billy Bob Joe";
+
+        System.out.println("Name: " + name);
+        System.out.println("All UPPER case: " + name.toUpperCase());
+        System.out.println("First character: " + name.charAt(0));
+        System.out.println("Length: " + name.length());
+        System.out.println("Last character: " + name.charAt(name.length()-1));
+        System.out.println("Substring: " + name.substring(10, 13));
+
+
+    }
+}
