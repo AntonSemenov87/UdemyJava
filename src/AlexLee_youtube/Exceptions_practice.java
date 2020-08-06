e6ddc10b123662264b2af7eafd32f80b6bbd0766
AntonSemenov87 <Anthony04061987@gmail.com>
2020-08-05 22:12:46 -0500

    exceptions

---
+++ src/AlexLee_youtube/Exceptions_practice.java
@@ -0,0 +1,13 @@
+package AlexLee_youtube;
+
+import java.util.ArrayList;
+
+public class Exceptions_practice {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.get(0);
+
+
+    }
+}
