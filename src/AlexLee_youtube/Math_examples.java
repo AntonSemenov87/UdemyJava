5a606e46cfeea4ee51bf22da46efc1da792302c5
AntonSemenov87 <Anthony04061987@gmail.com>
2020-08-17 22:17:43 -0500

    Math practice

--- src/AlexLee_youtube/Math_examples.java
+++ src/AlexLee_youtube/Math_examples.java
@@ -12,7 +12,13 @@ public class Math_examples {
         System.out.println(Math.addExact(3, 4));
         System.out.println(Math.floorDiv(5, 2)); // 5/2=2.5 --> 2
         System.out.println(Math.floorMod(5, 2)); // EQUAL TO MODULUS 5%2 --> remainder is 1
+        System.out.println(Math.incrementExact(8)); // returns 9 (8 + 1)
 
+        System.out.println(Math.max(4, 35)); // 35 --> returns max of two values
+        System.out.println(Math.multiplyExact(2, 3)); // multiplying --> 2 x 3 = 6
+        System.out.println(Math.pow(3, 4)); // calculates exponents
+        System.out.println(Math.random()); // random number between 0 and 1
+        System.out.println(Math.rint(4.5)); // rounds to 4 from double to int
 
 
     }
