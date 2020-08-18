bd0556db1dced9193a6282726190678cbf7d2983
AntonSemenov87 <Anthony04061987@gmail.com>
2020-08-17 22:02:19 -0500

    Math practice

---
+++ src/AlexLee_youtube/Math_examples.java
@@ -0,0 +1,19 @@
+package AlexLee_youtube;
+
+public class Math_examples {
+    public static void main(String[] args) {
+
+        System.out.println(Math.class);
+        System.out.println(Math.E);
+        System.out.println(Math.PI);
+        System.out.println(Math.abs(-12));
+        System.out.println(Math.acos(.54)); // between 0 and 1 --> 1.000359....
+        System.out.println(Math.acos(1.1)); // NaN --> not a number
+        System.out.println(Math.addExact(3, 4));
+        System.out.println(Math.floorDiv(5, 2)); // 5/2=2.5 --> 2
+        System.out.println(Math.floorMod(5, 2)); // EQUAL TO MODULUS 5%2 --> remainder is 1
+
+
+
+    }
+}
