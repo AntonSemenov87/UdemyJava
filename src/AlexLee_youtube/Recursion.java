48f6ce72cf09d6572310b7e54333027e6058ed4a
AntonSemenov87 <Anthony04061987@gmail.com>
2020-09-01 20:04:37 -0500

    recursion

--- src/AlexLee_youtube/Recursion.java
+++ src/AlexLee_youtube/Recursion.java
@@ -2,6 +2,19 @@ package AlexLee_youtube;
 
 public class Recursion {
     public static void main(String[] args) {
+        sayHi(5);
+
 
     }
+
+    public static void sayHi (int n) {
+        if (n == 0) {
+            System.out.println("Done!");
+        } else {
+            System.out.println("Hi");
+            n--;
+            sayHi(n);
+        }
+    }
+
 }
