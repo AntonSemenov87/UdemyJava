b2bc2faafdad92a36970499281b4ae069ecb18bb
AntonSemenov87 <Anthony04061987@gmail.com>
2020-09-01 20:09:47 -0500

    recursion

--- src/AlexLee_youtube/Recursion_ex2.java
+++ src/AlexLee_youtube/Recursion_ex2.java
@@ -1,20 +1,19 @@
 package AlexLee_youtube;
 
-public class Recursion {
+public class Recursion_ex2 {
     public static void main(String[] args) {
-        sayHi(5);
 
+        countBackwards(6);
 
     }
 
-    public static void sayHi (int n) {
+    public static void countBackwards (int n) {
         if (n == 0) {
             System.out.println("Done!");
         } else {
-            System.out.println("Hi");
+            System.out.println(n);
             n--;
-            sayHi(n);
+            countBackwards(n);
         }
     }
-
 }
