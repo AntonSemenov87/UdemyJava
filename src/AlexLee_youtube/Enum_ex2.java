047eea14be8d23394e1526578517386f6b4b9a45
AntonSemenov87 <Anthony04061987@gmail.com>
2020-09-01 20:35:29 -0500

    Enums practice

---
+++ src/AlexLee_youtube/Enum_ex2.java
@@ -0,0 +1,21 @@
+package AlexLee_youtube;
+
+public class Enum_ex2 {
+
+    enum Flavor {
+        CHOCOLATE, VANILLA, STRAWBERRY;
+    }
+
+    public static void main(String[] args) {
+
+        Flavor flav = Flavor.VANILLA;
+
+        if (flav == Flavor.VANILLA) {
+            System.out.println("It's vanilla");
+        } else if (flav == Flavor.CHOCOLATE) {
+            System.out.println("It's chocolate");
+        } else if (flav == Flavor.STRAWBERRY){
+            System.out.println("it's strawberry");
+        }
+    }
+}
