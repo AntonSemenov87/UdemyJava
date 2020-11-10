136533c61b558c25dcf65fafb04db22020e3db94
AntonSemenov <Anthony04061987@gmail.com>
2020-11-09 18:42:25 -0600

    Classes, Objects

--- src/Cyber_practice/constructors/Sky.java
+++ src/Cyber_practice/constructors/Sky.java
@@ -2,16 +2,32 @@ package Cyber_practice.constructors;
 
 public class Sky {
 
-    public Sky(){
-        System.out.println("Hi there!");
+    public Sky(String color){
+        this.color = color;
+    }
+
+    public Sky(String color, int visibleStars) {
+        this.color = color;
+        this.visibleStars = visibleStars;
+    }
+
+    public void info () {
+        System.out.println("Color: " + color);
+        System.out.println("Number of visible stars: " + visibleStars);
     }
 
     String color;
     int visibleStars;
 
     public static void main(String[] args) {
-        Sky refSky = new Sky();
+        Sky refSky = new Sky("blue");
+        System.out.println(refSky.color);
 
+        Sky refSky1 = new Sky("dark blue", 345);
+        //System.out.println(refSky1.color);
+        //System.out.println(refSky1.visibleStars);
+
+        refSky1.info();
     }
 
 }
