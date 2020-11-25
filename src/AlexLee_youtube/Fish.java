3245d3f22a829c9a4a757e5d1ec6e5b009708ca9
AntonSemenov87 <Anthony04061987@gmail.com>
2020-11-25 17:09:38 -0600

    Some practice

---
+++ src/AlexLee_youtube/Fish.java
@@ -0,0 +1,29 @@
+package AlexLee_youtube;
+
+public class Fish {
+    String breed;
+    String name;
+    int age;
+    double weight;
+
+    public Fish setBreed(String breed) {
+        this.breed = breed;
+        return this;
+    }
+
+    public Fish setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Fish setAge (int age) {
+        this.age = age;
+        return this;
+    }
+
+    public Fish setWeight (double weight) {
+        this.weight = weight;
+        return this;
+    }
+
+}
--- src/RebootCamp/NewClass.java
+++ src/RebootCamp/NewClass.java
@@ -1,7 +1,22 @@
 package RebootCamp;
 
+import java.util.HashMap;
+
 public class NewClass {
     public static void main(String[] args) {
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("A", 1);
+        map.put("B", 2);
+        map.put("C", 3);
+        map.put("C", 4);
+        map.put("C", 3);
+        map.put(null, 5);
+        System.out.println(map);
+
+        for (Integer each: map.values()) {
+            System.out.println(each);
+        }
 
     }
 }
